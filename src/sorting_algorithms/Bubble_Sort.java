6036aff8c0eb6d90ad152a2211993e11cbf00add
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-03-12 14:50:05 +0530

    second commit

--- src/sorting_algorithms/Bubble_Sort.java
+++ src/sorting_algorithms/Bubble_Sort.java
@@ -3,7 +3,11 @@ package sorting_algorithms;
 public class Bubble_Sort {
     public static void main(String[] args){
         int[] array = {12, -1, -100, 35, 1000, 96, 256, 900, 0};
-        int [] answer = sort(array, array.length);
+//        int [] answer = sort(array, array.length);
+//        for(int i : answer){
+//            System.out.print(i + " ");
+//        }
+        int [] answer = sortWithOptimization(array, array.length);
         for(int i : answer){
             System.out.print(i + " ");
         }
@@ -22,6 +26,29 @@ public class Bubble_Sort {
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
+            }
+        }
+        return array;
+    }
+
+    //Bubble - Sort With Optimization:
+    private static int[] sortWithOptimization(int[] array, int length) {
+
+        //Rounds:
+        for(int i = 0 ; i < length - 1 ; i++){
+            int temp;
+            boolean swapped = false;
+            //Processing Elements Till (length - i):
+            for(int j = 0 ; j < length - i - 1 ; j++){
+                if(array[j] > array[j + 1]){
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+
+            if(swapped == false){
+                break;
             }
         }
         return array;
