c1cbf8ebba8661c94fae1cb920c17e5c9d081265
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-03-12 23:10:43 +0530

    second commit

---
+++ src/sorting_algorithms/Insertion_Sort.java
@@ -0,0 +1,34 @@
+package sorting_algorithms;
+
+public class Insertion_Sort {
+
+    public static void main(String[] args){
+        int[] array = {12, -1, -100, 35, 1000, 96, 256, 900, 0};
+        int [] answer = sort(array, array.length);
+        for(int i : answer){
+            System.out.print(i + " ");
+        }
+    }
+
+    //Insertion - Sort:
+    private static int[] sort(int[] array, int length) {
+
+        //Rounds:
+        for(int i = 1 ; i < length ; i++){
+            //Current - Element:
+            int currentElement = array[i];
+            //Previous Sub Array:(i - 1 To 0)
+            int j = i - 1;
+            for( ; j >= 0 ; j--){
+                //Current Element Is Smaller Than array[j]:
+                if(currentElement < array[j]){
+                    //Shift Ahead:
+                    array[j + 1] = array[j];
+                }else break;
+            }
+            //Put Element At The Right Place:
+            array[j + 1] = currentElement;
+        }
+        return array;
+    }
+}
