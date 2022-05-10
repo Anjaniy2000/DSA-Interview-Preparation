8038f6f12d8925a7a5f067318b49159c63896590
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-05-10 23:11:31 +0530

    seventh commit

--- src/arrays/Day2/MoveZeros.java
+++ src/arrays/Day2/MoveZeros.java
@@ -52,6 +52,7 @@ public class MoveZeros {
         int j = 0;
         for(int i = 0 ; i < size ; i++){
             if(array[i] != 0){
+                //Optimization:
                 if(array[j] != 0){
                     j++;
                 }
