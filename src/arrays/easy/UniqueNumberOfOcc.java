3c390e9ba3294b529aa8a70af995a4d78ab332a1
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-06-13 23:34:30 +0530

    tenth commit

--- src/arrays/easy/UniqueNumberOfOcc.java
+++ src/arrays/easy/UniqueNumberOfOcc.java
@@ -5,8 +5,12 @@ import java.util.HashSet;
 
 public class UniqueNumberOfOcc {
     public static void main(String[] args) {
-        int[] array = {1,2,2,1,1,3};
+//        int[] array = {1,2,2,1,1,3};
+        int[] tempArray = {1,1,2,3,4,5,5,7,7,7,8};
+        solution(tempArray);
+    }
 
+    private static void solution(int[] array) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int element: array){
             map.put(element, map.getOrDefault(element, 0) + 1);
